fbfff429f14ddc3dbf9fa90caf1f5f20b6ccd39b
Franck <fkdiomande@icloud.com>
2025-10-26 13:06:07 +0100

    Add routing package

---
+++ packages/routing/src/count.ts
@@ -0,0 +1,3 @@
+export function countRoutes(): number {
+  return 3;
+}
---
+++ packages/routing/src/types.ts
@@ -0,0 +1,18 @@
+const METHODS = [
+  "GET",
+  "POST",
+  "PUT",
+  "DELETE",
+  "PATCH",
+  "OPTIONS",
+  "HEAD",
+] as const;
+
+export type MethodType = (typeof METHODS)[number];
+
+export type RouteConfigType = {
+  name: string;
+  path: `/${string}`;
+  method: MethodType;
+  description?: string;
+};

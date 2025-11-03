dd63a225f80ee7303dd567da4681cc3e28b8e74e
Franck <fkdiomande@icloud.com>
2025-11-03 08:22:20 +0100

    fix(types): Refactors types package
    Removes dependencies from the types package and defines standalone types.
    This change aims to reduce the package size and avoid unnecessary dependencies
    for consumers who only need the type definitions. It separates types from
    concrete implementations, enhancing modularity and reusability.

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,27 +1,3 @@
-export type { IAnalytics, PostHogAdapterCaptureType } from "@ooneex/analytics";
-export type { EContainerScope, IContainer } from "@ooneex/container";
-export type { ExceptionStackFrameType, IException } from "@ooneex/exception";
-export type {
-  HeaderFieldType,
-  IHeader,
-  IReadonlyHeader,
-  IUserAgent,
-  UserAgentBrowserType,
-  UserAgentCpuType,
-  UserAgentDeviceType,
-  UserAgentEngineType,
-  UserAgentOsType,
-  UserAgentType,
-} from "@ooneex/http-header";
-export type { IMime, MimeType } from "@ooneex/http-mimes";
-export type {
-  IStatus,
-  StatusCodeType,
-  StatusTextType,
-} from "@ooneex/http-status";
 export type HttpMethodType = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "OPTIONS" | "HEAD";
 export type EncodingType = "deflate" | "gzip" | "compress" | "br" | "identity" | "*";
 export type CharsetType = "ISO-8859-1" | "7-BIT" | "UTF-8" | "UTF-16" | "US-ASCII";
-
-export * from "./repository";
-export * from "./response";

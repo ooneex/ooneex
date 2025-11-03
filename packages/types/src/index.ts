1434d7d9e3cfc209059b661bb5cd361862b833e9
Franck <fkdiomande@icloud.com>
2025-11-03 13:47:19 +0100

    feat(types): Define ScalarType

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,3 +1,4 @@
 export type HttpMethodType = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "OPTIONS" | "HEAD";
 export type EncodingType = "deflate" | "gzip" | "compress" | "br" | "identity" | "*";
 export type CharsetType = "ISO-8859-1" | "7-BIT" | "UTF-8" | "UTF-16" | "US-ASCII";
+export type ScalarType = boolean | number | bigint | string;

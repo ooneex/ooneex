4887b8ff85ee2fc078bb139d1b3c036d7b193ec5
Franck <fkdiomande@icloud.com>
2025-11-08 18:39:01 +0100

    feat(logger): Add ILogger interface to types

---
+++ packages/logger/src/types.ts
@@ -0,0 +1,9 @@
+export interface ILogger {
+  log: (...message: string[]) => void;
+  fatal: (...message: string[]) => void;
+  error: (...message: string[]) => void;
+  warn: (...message: string[]) => void;
+  info: (...message: string[]) => void;
+  debug: (...message: string[]) => void;
+  trace: (...message: string[]) => void;
+}

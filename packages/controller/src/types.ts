6e8e5c97908196f615e7dca67c4410ce5daf29c3
Franck <fkdiomande@icloud.com>
2025-11-12 08:49:01 +0100

    feat(controller): Add cache dependency to controller package

--- packages/controller/src/types.ts
+++ packages/controller/src/types.ts
@@ -1,8 +1,10 @@
 import type { IAnalytics } from "@ooneex/analytics";
+import type { ICache } from "@ooneex/cache";
 import type { ILogger, LogsEntity } from "@ooneex/logger";
 import type { ScalarType } from "@ooneex/types";
 
 export type ActionType = {
   logger: ILogger<Record<string, ScalarType>> | ILogger<LogsEntity>;
   analytics?: IAnalytics;
+  cache?: ICache;
 };

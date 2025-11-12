6295b838517da517be3e4efc8c3bb05e223ab47a
Franck <fkdiomande@icloud.com>
2025-11-12 08:26:52 +0100

    feat(controller): Add analytics dependency to controller package

--- packages/controller/src/types.ts
+++ packages/controller/src/types.ts
@@ -1,6 +1,8 @@
+import type { IAnalytics } from "@ooneex/analytics";
 import type { ILogger, LogsEntity } from "@ooneex/logger";
 import type { ScalarType } from "@ooneex/types";
 
 export type ActionType = {
   logger: ILogger<Record<string, ScalarType>> | ILogger<LogsEntity>;
+  analytics?: IAnalytics;
 };

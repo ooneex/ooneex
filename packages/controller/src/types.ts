7e6993b1dcdff58dd628ce5eca9488a414692876
Franck <fkdiomande@icloud.com>
2025-11-12 07:51:08 +0100

    feat(controller): Add controller package with initial types and dependencies

---
+++ packages/controller/src/types.ts
@@ -0,0 +1,6 @@
+import type { ILogger, LogsEntity } from "@ooneex/logger";
+import type { ScalarType } from "@ooneex/types";
+
+export type ActionType = {
+  logger: ILogger<Record<string, ScalarType>> | ILogger<LogsEntity>;
+};

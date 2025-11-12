58e36232139a035e47fc183835f53c90f942157f
Franck <fkdiomande@icloud.com>
2025-11-12 09:08:32 +0100

    feat(controller): Add storage dependency to controller package

--- packages/controller/src/types.ts
+++ packages/controller/src/types.ts
@@ -1,10 +1,12 @@
 import type { IAnalytics } from "@ooneex/analytics";
 import type { ICache } from "@ooneex/cache";
 import type { ILogger, LogsEntity } from "@ooneex/logger";
+import type { IStorage } from "@ooneex/storage";
 import type { ScalarType } from "@ooneex/types";
 
 export type ActionType = {
   logger: ILogger<Record<string, ScalarType>> | ILogger<LogsEntity>;
   analytics?: IAnalytics;
   cache?: ICache;
+  storage?: IStorage;
 };

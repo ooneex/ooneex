47a1a1583288deeba13ba953549a69bd4db3c0d9
Franck <fkdiomande@icloud.com>
2025-11-13 06:40:06 +0100

    feat(controller): Add database dependency to controller package

--- packages/controller/src/types.ts
+++ packages/controller/src/types.ts
@@ -1,5 +1,6 @@
 import type { IAnalytics } from "@ooneex/analytics";
 import type { ICache } from "@ooneex/cache";
+import type { IDatabase } from "@ooneex/database";
 import type { ILogger, LogsEntity } from "@ooneex/logger";
 import type { IStorage } from "@ooneex/storage";
 import type { ScalarType } from "@ooneex/types";
@@ -9,4 +10,5 @@ export type ActionType = {
   analytics?: IAnalytics;
   cache?: ICache;
   storage?: IStorage;
+  database?: IDatabase;
 };

5a28abae4aba03720b91cd5d621c5a9851609b5d
Franck <fkdiomande@icloud.com>
2025-11-16 15:19:14 +0100

    feat(color): Add @ooneex/color package

---
+++ packages/color/src/types.ts
@@ -0,0 +1,67 @@
+export enum EStatus {
+  IN_REVIEW = "inReview",
+  REVIEWED = "reviewed",
+  DONE = "done",
+  DELETE = "delete",
+  PENDING = "pending",
+  APPROVED = "approved",
+  REJECTED = "rejected",
+  ARCHIVED = "archived",
+}
+
+export type StatusType = `${EStatus}`;
+
+type HexDigitType =
+  | "0"
+  | "1"
+  | "2"
+  | "3"
+  | "4"
+  | "5"
+  | "6"
+  | "7"
+  | "8"
+  | "9"
+  | "a"
+  | "b"
+  | "c"
+  | "d"
+  | "e"
+  | "f"
+  | "A"
+  | "B"
+  | "C"
+  | "D"
+  | "E"
+  | "F";
+
+export type HexaType =
+  | `#${HexDigitType}${HexDigitType}${HexDigitType}`
+  // @ts-expect-error: trust me
+  | `#${HexDigitType}${HexDigitType}${HexDigitType}${HexDigitType}${HexDigitType}${HexDigitType}`;
+
+export type RGBType = `rgb(${number}, ${number}, ${number})`;
+
+export type RGBAType = `rgba(${number}, ${number}, ${number}, ${number})`;
+
+export type HSLType = `hsl(${number}, ${number}%, ${number}%)`;
+
+export type HSLAType = `hsla(${number}, ${number}%, ${number}%, ${number})`;
+
+export type ColorType = HexaType | RGBType | RGBAType | HSLType | HSLAType;
+
+export interface IColor {
+  hex?: HexaType;
+  rgb?: RGBType;
+  rgba?: RGBAType;
+  hsl?: HSLType;
+  hsla?: HSLAType;
+  value: ColorType;
+  alpha?: number;
+  red?: number;
+  green?: number;
+  blue?: number;
+  hue?: number;
+  saturation?: number;
+  lightness?: number;
+}

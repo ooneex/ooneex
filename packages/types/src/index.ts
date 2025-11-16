0180d196aa46dff1cf94e138a26173a54dc9b9c4
Franck <fkdiomande@icloud.com>
2025-11-16 16:03:47 +0100

    feat(types): Add IBase interface with common entity properties

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -2,3 +2,16 @@ export type HttpMethodType = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "OPTI
 export type EncodingType = "deflate" | "gzip" | "compress" | "br" | "identity" | "*";
 export type CharsetType = "ISO-8859-1" | "7-BIT" | "UTF-8" | "UTF-16" | "US-ASCII";
 export type ScalarType = boolean | number | bigint | string;
+
+export interface IBase {
+  isLocked?: boolean;
+  lockedAt?: Date;
+  isBlocked?: boolean;
+  blockedAt?: Date;
+  blockReason?: string;
+  isPublic?: boolean;
+  isActive?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
+}

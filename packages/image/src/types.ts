0c27d717067b5fdf0e0b58bf867e24c37e04ef49
Franck <fkdiomande@icloud.com>
2025-11-17 12:44:04 +0100

    refactor(image): Convert image format and mime types to const arrays

--- packages/image/src/types.ts
+++ packages/image/src/types.ts
@@ -2,34 +2,40 @@ import type { IStatus } from "@ooneex/status";
 import type { ITag } from "@ooneex/tag";
 import type { IBase, ScalarType } from "@ooneex/types";
 
-export type ImageFormatType =
-  | "jpeg"
-  | "jpg"
-  | "png"
-  | "webp"
-  | "gif"
-  | "svg"
-  | "avif"
-  | "bmp"
-  | "tiff"
-  | "ico"
-  | "heic"
-  | "heif";
+export const IMAGE_FORMATS = [
+  "jpeg",
+  "jpg",
+  "png",
+  "webp",
+  "gif",
+  "svg",
+  "avif",
+  "bmp",
+  "tiff",
+  "ico",
+  "heic",
+  "heif",
+] as const;
 
-export type ImageMimeType =
-  | "image/jpeg"
-  | "image/png"
-  | "image/webp"
-  | "image/gif"
-  | "image/svg+xml"
-  | "image/avif"
-  | "image/bmp"
-  | "image/x-ms-bmp"
-  | "image/tiff"
-  | "image/vnd.microsoft.icon"
-  | "image/x-icon"
-  | "image/heic"
-  | "image/heif";
+export type ImageFormatType = (typeof IMAGE_FORMATS)[number];
+
+export const IMAGE_MIMES = [
+  "image/jpeg",
+  "image/png",
+  "image/webp",
+  "image/gif",
+  "image/svg+xml",
+  "image/avif",
+  "image/bmp",
+  "image/x-ms-bmp",
+  "image/tiff",
+  "image/vnd.microsoft.icon",
+  "image/x-icon",
+  "image/heic",
+  "image/heif",
+] as const;
+
+export type ImageMimeType = (typeof IMAGE_MIMES)[number];
 
 export interface IImage extends IBase {
   url: string;

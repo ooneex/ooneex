e77aaeb6b64e418ff177f0f1f173b734f0138086
Franck <fkdiomande@icloud.com>
2025-11-17 21:39:15 +0100

    refactor(common): Restructure packages and refactor monorepo organization

--- packages/entities/README.md
+++
@@ -1,1 +0,0 @@
-# @ooneex/cron
--- packages/entities/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { ColorEntity } from "./ColorEntity";
-export { ImageEntity } from "./ImageEntity";
-export { StatusEntity } from "./StatusEntity";
-export { TagEntity } from "./TagEntity";
-// export * from "./types";

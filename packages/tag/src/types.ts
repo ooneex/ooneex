20ed157c9827bfc8cfc452e947e59c89453be5fe
Franck <fkdiomande@icloud.com>
2025-11-17 19:01:43 +0100

    feat(tag): Add optional metadata field to ITag interface

--- packages/tag/src/types.ts
+++ packages/tag/src/types.ts
@@ -1,7 +1,8 @@
 import type { IColor } from "@ooneex/color";
-import type { IBase } from "@ooneex/types";
+import type { IBase, ScalarType } from "@ooneex/types";
 
 export interface ITag extends IBase {
   name: string;
   color?: IColor;
+  metadata?: Record<string, ScalarType>;
 }

ec62040a26e76dd0d74a42a68c00bb8db6eb2143
Franck <fkdiomande@icloud.com>
2025-11-17 21:38:38 +0100

    refactor(types): Add language field to IBase interface

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,3 +1,5 @@
+import type { LocaleType } from "@ooneex/translation";
+
 export type HttpMethodType = "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "OPTIONS" | "HEAD";
 export type EncodingType = "deflate" | "gzip" | "compress" | "br" | "identity" | "*";
 export type CharsetType = "ISO-8859-1" | "7-BIT" | "UTF-8" | "UTF-16" | "US-ASCII";
@@ -14,4 +16,5 @@ export interface IBase {
   createdAt?: Date;
   updatedAt?: Date;
   deletedAt?: Date;
+  language?: LocaleType;
 }

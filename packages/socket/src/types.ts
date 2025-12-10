9cad01614bd4abcb3bb822940e700011d72a81b5
Franck <fkdiomande@icloud.com>
2025-12-10 16:28:55 +0100

    feat(socket): Add TypeScript types and dependencies to socket package

---
+++ packages/socket/src/types.ts
@@ -0,0 +1,62 @@
+import type { IAnalytics } from "@ooneex/analytics";
+import type { IAppEnv } from "@ooneex/app-env";
+import type { ICache } from "@ooneex/cache";
+import type { IDatabase, IRedisDatabaseAdapter } from "@ooneex/database";
+import type { Header } from "@ooneex/http-header";
+import type { RequestConfigType } from "@ooneex/http-request";
+import type { IRequestFile } from "@ooneex/http-request-file";
+import type { IResponse } from "@ooneex/http-response";
+import type { ILogger, LogsEntity } from "@ooneex/logger";
+import type { IMailer } from "@ooneex/mailer";
+import type { IPermission } from "@ooneex/permission";
+import type { IStorage } from "@ooneex/storage";
+import type { LocaleInfoType } from "@ooneex/translation";
+import type { ScalarType } from "@ooneex/types";
+import type { IUrl } from "@ooneex/url";
+import type { IUser } from "@ooneex/user";
+
+// biome-ignore lint/suspicious/noExplicitAny: trust me
+export type ControllerClassType = new (...args: any[]) => IController;
+
+export interface IController<T extends ContextConfigType = ContextConfigType> {
+  index: (context: ContextType<T>) => Promise<IResponse<T["response"]>> | IResponse<T["response"]>;
+}
+
+export type ContextConfigType = {
+  response: Record<string, unknown>;
+  request: RequestConfigType;
+};
+
+export type RequestDataType = {
+  params?: Record<string, string | number>;
+  payload?: Record<string, unknown>;
+  queries?: Record<string, string | number>;
+  language?: LocaleInfoType;
+};
+
+export type ContextType<T extends ContextConfigType = ContextConfigType> = {
+  logger: ILogger<Record<string, ScalarType>> | ILogger<LogsEntity>;
+  analytics?: IAnalytics;
+  cache?: ICache;
+  permission?: IPermission;
+  storage?: IStorage;
+  database?: IDatabase;
+  redis?: IRedisDatabaseAdapter;
+  mailer?: IMailer;
+  app: {
+    url: IAppEnv;
+    env: IAppEnv;
+  };
+  response: IResponse<T["response"]>;
+  path: string;
+  url: IUrl;
+  header: Header;
+  params: T["request"]["params"];
+  payload: T["request"]["payload"];
+  queries: T["request"]["queries"];
+  files: Record<string, IRequestFile>;
+  ip: string | null;
+  host: string;
+  language: LocaleInfoType;
+  user: IUser | null;
+};

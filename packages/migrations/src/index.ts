709b4853a117b49ccce4bfb9702c6e9cf96fda1b
Franck <fkdiomande@icloud.com>
2025-12-12 16:51:08 +0100

    feat(migrations): Export migrationUp function

--- packages/migrations/src/index.ts
+++ packages/migrations/src/index.ts
@@ -3,4 +3,5 @@ export { migration } from "./decorators";
 export { generateMigrationVersion } from "./generateMigrationVersion";
 export { getMigrations } from "./getMigrations";
 export { migrationCreate } from "./migrationCreate";
+export { migrationUp } from "./migrationUp";
 export * from "./types";

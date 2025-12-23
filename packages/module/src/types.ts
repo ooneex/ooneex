ba21e5bb05ffd6b74dc5e3515e28c00570d3289b
Franck <fkdiomande@icloud.com>
2025-12-23 20:35:08 +0200

    feat(common): Add cronJobs to ModuleType

--- packages/module/src/types.ts
+++ packages/module/src/types.ts
@@ -1,7 +1,9 @@
 import type { ControllerClassType } from "@ooneex/controller";
+import type { CronClassType } from "@ooneex/cron";
 import type { EntityClassType } from "@ooneex/entity";
 
 export type ModuleType = {
   controllers: ControllerClassType[];
+  cronJobs: CronClassType[];
   entities: EntityClassType[];
 };

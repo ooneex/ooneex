7a1f5d94886948aeaff92378264db64ef52e70a7
Franck <fkdiomande@icloud.com>
2026-01-02 19:24:32 +0200

    refactor(app): Rename ITypeormDatabaseAdapter to ITypeormDatabase

--- packages/app/src/types.ts
+++ packages/app/src/types.ts
@@ -1,7 +1,7 @@
 import type { AnalyticsClassType } from "@ooneex/analytics";
 import type { IAppEnv } from "@ooneex/app-env";
 import type { CacheClassType } from "@ooneex/cache";
-import type { IDatabase, ITypeormDatabaseAdapter } from "@ooneex/database";
+import type { IDatabase, ITypeormDatabase } from "@ooneex/database";
 import type { LoggerClassType } from "@ooneex/logger";
 import type { MailerClassType } from "@ooneex/mailer";
 import type { StorageClassType } from "@ooneex/storage";
@@ -12,7 +12,7 @@ export type AppConfigType = {
   cache?: CacheClassType;
   storage?: StorageClassType;
   mailer?: MailerClassType;
-  database?: IDatabase | ITypeormDatabaseAdapter;
+  database?: IDatabase | ITypeormDatabase;
   env: IAppEnv;
   // TODO: Add cronJobs
 };

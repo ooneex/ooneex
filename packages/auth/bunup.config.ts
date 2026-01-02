7055b250701c5bef66ce12fea530ba74edaa438f
Franck <fkdiomande@icloud.com>
2026-01-02 22:54:39 +0200

    build(auth): Change build target from browser to bun

--- packages/auth/bunup.config.ts
+++ packages/auth/bunup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "bunup";
 import { copy } from "bunup/plugins";
 
 export default defineConfig({
-  target: "browser",
+  target: "bun",
   format: ["esm"],
   drop: ["console", "debugger"],
   packages: "external",

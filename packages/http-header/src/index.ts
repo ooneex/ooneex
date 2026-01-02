10762cf7701faeeea1366b13e93534c99d006f40
Franck <fkdiomande@icloud.com>
2026-01-02 18:38:27 +0200

    refactor(http-header): Use named exports in index

--- packages/http-header/src/index.ts
+++ packages/http-header/src/index.ts
@@ -1,15 +1,3 @@
 export { Header } from "./Header";
 export { ReadonlyHeader } from "./ReadonlyHeader";
-
-export type {
-  HeaderFieldType,
-  IHeader,
-  IReadonlyHeader,
-  IUserAgent,
-  UserAgentBrowserType,
-  UserAgentCpuType,
-  UserAgentDeviceType,
-  UserAgentEngineType,
-  UserAgentOsType,
-  UserAgentType,
-} from "./types";
+export * from "./types";

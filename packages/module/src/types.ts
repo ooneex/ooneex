6b177ecfb9f2fdf0c32f16d901f42c81b2fd708b
Franck <fkdiomande@icloud.com>
2026-01-03 16:40:16 +0200

    refactor(model): Remove cronJobs from ModuleType

--- packages/module/src/types.ts
+++ packages/module/src/types.ts
@@ -1,9 +1,7 @@
 import type { ControllerClassType } from "@ooneex/controller";
-import type { CronClassType } from "@ooneex/cron";
 import type { EntityClassType } from "@ooneex/entity";
 
 export type ModuleType = {
   controllers: ControllerClassType[];
-  cronJobs: CronClassType[];
   entities: EntityClassType[];
 };

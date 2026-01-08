625d0269e32d6d726318cd3db5c62032bcc93f45
Franck <fkdiomande@icloud.com>
2026-01-08 12:35:36 +0200

    refactor(controller): Remove redis and permission from context type

--- packages/controller/src/types.ts
+++ packages/controller/src/types.ts
@@ -1,14 +1,13 @@
 import type { IAnalytics } from "@ooneex/analytics";
 import type { IAppEnv } from "@ooneex/app-env";
 import type { ICache } from "@ooneex/cache";
-import type { IDatabase, IRedisDatabase } from "@ooneex/database";
+import type { IDatabase } from "@ooneex/database";
 import type { Header } from "@ooneex/http-header";
 import type { IRequest, RequestConfigType } from "@ooneex/http-request";
 import type { IRequestFile } from "@ooneex/http-request-file";
 import type { IResponse } from "@ooneex/http-response";
 import type { ILogger, LogsEntity } from "@ooneex/logger";
 import type { IMailer } from "@ooneex/mailer";
-import type { IPermission } from "@ooneex/permission";
 import type { IStorage } from "@ooneex/storage";
 import type { LocaleInfoType } from "@ooneex/translation";
 import type { HttpMethodType, ScalarType } from "@ooneex/types";
@@ -29,13 +28,10 @@ export type ContextType<T extends ContextConfigType = ContextConfigType> = {
   logger: ILogger<Record<string, ScalarType>> | ILogger<LogsEntity>;
   analytics?: IAnalytics;
   cache?: ICache;
-  permission?: IPermission;
   storage?: IStorage;
   database?: IDatabase;
-  redis?: IRedisDatabase;
   mailer?: IMailer;
   app: {
-    url: string;
     env: IAppEnv;
   };
   response: IResponse<T["response"]>;
